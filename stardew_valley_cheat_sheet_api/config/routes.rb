ce65436765b6b82f795d19bff9c384b184f03a8e
breannams <breannamarie_s@hotmail.com>
2021-09-24 18:33:22 -0400

    Add backend functionality for JWT Auth.

--- stardew_valley_cheat_sheet_api/config/routes.rb
+++ stardew_valley_cheat_sheet_api/config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   resources :buildings
   resources :villagers
   resources :games
-  resources :users
+  
+  resources :users, only: [:create]
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
