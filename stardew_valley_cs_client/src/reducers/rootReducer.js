19e34ab267831be477b762268b9735d3a065fd72
breannams <breannamarie_s@hotmail.com>
2021-10-04 21:14:31 -0400

    import reducers into root reducer.

--- stardew_valley_cs_client/src/reducers/rootReducer.js
+++ stardew_valley_cs_client/src/reducers/rootReducer.js
@@ -1,9 +1,23 @@
 import {combineReducers} from 'redux'
+import farmsReducer from './farmsReducer'
 import  gamesReducer  from './gamesReducer'
+import animalsReducer from './animalsReducer'
+import buildingsReducer from './buildingsReducer'
+import cropsReducer from './buildingsReducer'
+import fesivalsReducer from './festivalsReducer'
+import questsReducer from './questsReducer'
+import villagersReducer from './villagersReducer'
 
 export  const rootReducer = combineReducers({
 
     games: gamesReducer,
+    animals: animalsReducer,
+    buildings: buildingsReducer,
+    crops: cropsReducer,
+    farms: farmsReducer,
+    festivals: fesivalsReducer,
+    quests: questsReducer,
+    villagers: villagersReducer
 
 })
 
