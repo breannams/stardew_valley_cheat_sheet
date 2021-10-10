5bd4795430f1eb0fdb3fe310bc26b9b16e7044ef
breannams <breannamarie_s@hotmail.com>
2021-10-09 23:09:10 -0400

    Add more to about page.

--- stardew_valley_cs_client/src/components/Pages/AboutPage.js
+++ stardew_valley_cs_client/src/components/Pages/AboutPage.js
@@ -10,12 +10,19 @@ render (){
             ______________________________________
         <br></br>
         <br></br>
-                Hello! To use this page please login / sign up first! Afterwards you can create your own farm and learn about different villagers, buildings, etc.
+                Hello! To use this page please login / sign up first! Afterwards you can create your own farm and learn about different villagers, buildings, etc. from Stardew Valley.
                 Future updates will (hopefully ;) )include:
+                <br></br>
                 - More info about game components!
+                <br></br>
                 - Ability to track villager hearts and get notified when you have a heart event ready!
+                <br></br>
                 - Ability to choose between Stardew Valley info and Stardew Valley Expanded info! (Yay for mods!)
+                <br></br>
                 - Ability to make and track more than one farm (that way if you're like me and have 1234802938012billion games going at once you can track all of them!)
+                <br></br>
+                -Pictures for game components and background changes
+        
         <br></br>
         <br></br>
    
